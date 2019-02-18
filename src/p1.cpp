cc6f1d995b571817c45fbddf0795903ca2af1fc9
nakzz <naqibprio10@gmail.com>
2019-02-18 17:50:18 -0600

    Task 3 implemented

--- src/p1.cpp
+++ src/p1.cpp
@@ -23,20 +23,29 @@ int main() {
 	//empty vector created
 	vector<int> v;
 	int inKey;
-	int inIndex;
+	signed inIndex;
 
 	v = task1(v);
 	cout << " The elements of the container are" << endl;
 	printVector(v);
 
+//	cout << "Enter the value of the element you want to enter" << endl;
+//	cin >> inKey;
+//	cout << "enter the position" << endl;
+//	cin >> inIndex;
+//
+//	v = task2(v, inKey, inIndex);
+//	cout << " the vector after insertion of a new value is" << endl;
+//	printVector(v);
 
-	cout << "Enter the value of the element you want to enter" << endl;
-	cin >> inKey;
-	cout << "enter the position" << endl;
+	cout << "Enter the position of the element you want to delete" << endl;
 	cin >> inIndex;
+	v = task3(v, inIndex);
+	printVector(v);
 
-	v = task2(v, inKey, inIndex);
-	cout << " the vector after insertion of a new value is" << endl;
+	cout << "Enter the position of the element you want to delete" << endl;
+	cin >> inIndex;
+	v = task3(v, inIndex);
 	printVector(v);
 
 	cout << "Enter the position of the element you want to delete" << endl;
@@ -51,7 +60,8 @@ int main() {
 }
 
 /**
- * Adds values to the vector
+ * Adds values to the vector, assuming that the user will only input integers
+ * and only input one per line
  *
  * param v is the vector values are being added too
  * return the vector that has been modified
@@ -80,19 +90,19 @@ vector<int> task1(vector<int> v) {
  * param index at location the value is being added
  * return the vector that has been modified
  */
-vector<int> task2(vector<int> v, int key, int index) {
+vector<int> task2(vector<int> v, int key, signed int index) {
 
 	vector<int>::iterator positionIterator;
 
-	cout << "adding new val to size of " << v.size() << endl;
+//	cout << "adding new val to size of " << v.size() << endl;
 
-	if(index >= v.size()) {
+	if (index >= v.size()) {
 		//if bigger than vector dimension
 		positionIterator = v.begin() + (v.size());
-	} else if(index >= 0 && index < v.size()){
+	} else if (index >= 0 && index < v.size()) {
 		//if within vector dimension
-		positionIterator = v.begin() +  index;
-	} else if (index < 0){
+		positionIterator = v.begin() + index;
+	} else if (index < 0) {
 		//if negative case
 		positionIterator = v.begin();
 	}
@@ -104,8 +114,38 @@ vector<int> task2(vector<int> v, int key, int index) {
 	return v;
 }
 
+/**
+ * Removes values to the vector at specific position
+ *
+ * param v is the vector values are being added too
+ * param index at location the value is being added
+ * return the vector that has been modified
+ */
+vector<int> task3(vector<int> v,signed int index) {
 
-vector<int> task3(vector<int> v, int index) {
+	if(v.size()==0){
+		return v;
+	}
+
+	if(index < 0){
+//		cout << "negative int found" << endl;
+		v.erase(v.begin());
+				return v;
+	}
+
+	vector<int>::iterator positionIterator;
+
+	if (index >= v.size()) {
+		//if bigger than vector dimension
+		positionIterator = v.begin() + (v.size() -1);
+	} else if (index >= 0 && index < v.size()) {
+		//if within vector dimension
+		positionIterator = v.begin() + index;
+	}
+
+	v.erase(positionIterator);
+
+	return v;
 }
 
 void task4(vector<int> v) {
