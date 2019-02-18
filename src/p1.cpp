3e1673a1b26cd50e947252a30e3825b1999164b2
nakzz <naqibprio10@gmail.com>
2019-02-18 01:47:52 -0600

    Driver method is finished

--- src/p1.cpp
+++ src/p1.cpp
@@ -14,26 +14,38 @@ using namespace std;
 
 vector<int> task1(vector<int> v); // Can take user input
 vector<int> task2(vector<int> v, int key, int index);
-void task3();
-void task4();
+vector<int> task3(vector<int> v, int index);
+void task4(vector<int> v);
 void printVector(vector<int> v);
 
 int main() {
 
 	//empty vector created
 	vector<int> v;
+	int inKey;
+	int inIndex;
 
 	v = task1(v);
+	cout << " The elements of the container are" << endl;
 	printVector(v);
 
-//	task2();
-//	printVector(v);
-//
-//	task3();
-//	printVector(v);
-//
-//	task4();
-//	printVector(v);
+	cout << "Enter the value of the element you want to enter" << endl;
+	cin >> inKey;
+	cout << "enter the position" << endl;
+	cin >> inIndex;
+
+	v= task2(v, inKey, inIndex );
+	cout << " the vector after insertion of a new value is" << endl;
+	printVector(v);
+
+	cout << "Enter the position of the element you want to delete" << endl;
+	cin >> inIndex;
+	v= task3(v, inIndex);
+	printVector(v);
+
+
+	task4(v);
+	printVector(v);
 
 	return 0;
 }
@@ -58,16 +70,18 @@ vector<int> task2(vector<int> v, int key, int index) {
 return v;
 }
 
-void task3() {
+vector<int> task3(vector<int> v, int index) {
 }
 
-void task4() {
+void task4(vector<int> v) {
+	cout << "Enter the position of the element you want to delete" << endl;
 }
 
 void printVector(vector<int> v) {
-	cout <<"printing " << endl;
+//	cout <<"printing " << endl;
 
 	for (int i = 0; i < v.size(); i++) {
-		cout <<"value at pos" << i << ": is "<< v.at(i) << endl;
+		cout <<"value at pos " << i << ": is "<< v.at(i) << endl;
 	}
+	cout << endl << endl;
 }
