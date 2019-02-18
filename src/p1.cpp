f9a8e1fd0487f3ba5f6404e612e42ffdf5498e1b
nakzz <naqibprio10@gmail.com>
2019-02-18 14:53:36 -0600

    Task 2 properly implemented

--- src/p1.cpp
+++ src/p1.cpp
@@ -29,20 +29,20 @@ int main() {
 	cout << " The elements of the container are" << endl;
 	printVector(v);
 
+
 	cout << "Enter the value of the element you want to enter" << endl;
 	cin >> inKey;
 	cout << "enter the position" << endl;
 	cin >> inIndex;
 
-	v= task2(v, inKey, inIndex );
+	v = task2(v, inKey, inIndex);
 	cout << " the vector after insertion of a new value is" << endl;
 	printVector(v);
 
 	cout << "Enter the position of the element you want to delete" << endl;
 	cin >> inIndex;
-	v= task3(v, inIndex);
+	v = task3(v, inIndex);
 	printVector(v);
-
 
 	task4(v);
 	printVector(v);
@@ -50,6 +50,12 @@ int main() {
 	return 0;
 }
 
+/**
+ * Adds values to the vector
+ *
+ * param v is the vector values are being added too
+ * return the vector that has been modified
+ */
 vector<int> task1(vector<int> v) {
 
 	int input;
@@ -66,9 +72,38 @@ vector<int> task1(vector<int> v) {
 
 }
 
+/**
+ * Adds values to the vector at specific position
+ *
+ * param v is the vector values are being added too
+ * param key value that is being added
+ * param index at location the value is being added
+ * return the vector that has been modified
+ */
 vector<int> task2(vector<int> v, int key, int index) {
-return v;
+
+	vector<int>::iterator positionIterator;
+
+	cout << "adding new val to size of " << v.size() << endl;
+
+	if(index >= v.size()) {
+		//if bigger than vector dimension
+		positionIterator = v.begin() + (v.size());
+	} else if(index >= 0 && index < v.size()){
+		//if within vector dimension
+		positionIterator = v.begin() +  index;
+	} else if (index < 0){
+		//if negative case
+		positionIterator = v.begin();
+	}
+
+	v.insert(positionIterator, 1, key);
+
+	cout << "added new val" << endl;
+
+	return v;
 }
+
 
 vector<int> task3(vector<int> v, int index) {
 }
@@ -81,7 +116,7 @@ void printVector(vector<int> v) {
 //	cout <<"printing " << endl;
 
 	for (int i = 0; i < v.size(); i++) {
-		cout <<"value at pos " << i << ": is "<< v.at(i) << endl;
+		cout << "value at pos " << i << ": is " << v.at(i) << endl;
 	}
 	cout << endl << endl;
 }
